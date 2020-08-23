f687d0293e051e46304258af8d0191d4cd6a0ce9
Jonathan Ramirez <jonathan.ramirez@tenpo.cl>
2020-08-22 22:21:54 -0400

    ADD: login with spotify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => console.log("USER => ", user))
+    }
+
+    console.log("TOKEN ==> ", hash)
+  }, [])
+
   return (
     <div className="app">
-      <h1>Spotify Clone project</h1>
+      {
+        token ? <h1>Logged IN</h1>
+          : (
+            <Login />
+          )
+      }
     </div>
   );
 }
