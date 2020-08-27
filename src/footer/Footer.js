80ecb9928ea8dd86d49759bdc2e6eb68c291f8cf
Jonathan Ramirez <jonathan.ramirez@tenpo.cl>
2020-08-26 21:14:21 -0400

    ADD: song playing footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import "./Footer.css"
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import { Grid, Slider } from '@material-ui/core';
+
 export default function Footer() {
     return (
         <div className="footer">
-            <h1>Footer</h1>
+            <div className="footer__left">
+                <img src="" className="footer__albumLogo" />
+                <div className="footer_songInfo">
+                    <h4>Yeah!</h4>
+                    <p>Usher</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon className="footer__green" />
+                <SkipPreviousIcon className="footer__icon" />
+                <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+                <SkipNextIcon className="footer__icon" />
+                <RepeatIcon className="footer__green" />
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon />
+                    </Grid>
+                    <Grid item xs>
+                        <Slider />
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
